c18907278fc7345390b85790736873bd41b5248b
Zhovtiak Maksym <maxon.zhovtyak@gmail.com>
2026-03-14 11:23:19 +0200

    Fixed problems with cloned repository.

---
+++ A5/mini-chatgpt/src/Lib.Sampling/Processing/ProbabilityNormalizer.cs
@@ -0,0 +1,23 @@
+﻿namespace MiniChatGPT.Sampling.Processing
+{
+    public static class ProbabilityNormalizer
+    {
+        public static void Normalize(float[] probs)
+        {
+            float sum = 0f;
+
+            for(int i = 0; i < probs.Length; i++)
+            {
+                sum += probs[i];
+            }
+
+            if(sum >  0f)
+            {
+                for(int i = 0; i < probs.Length;i++)
+                {
+                    probs[i] /= sum;
+                }
+            }
+        }
+    }
+}
---
+++ A5/mini-chatgpt/src/Lib.Sampling/Sampler.cs
@@ -0,0 +1,17 @@
+﻿using MiniChatGPT.Sampling.Interfaces;
+
+namespace MiniChatGPT.Sampling
+{
+    public class Sampler : ISampler
+    {
+        public int Sample(float[] probs, float temperature, int topK, Random? rng = null)
+        {
+
+        }
+
+        public int Sample(float[] probs, float temperature, int topK, int? seed)
+        {
+
+        }
+    }
+}
