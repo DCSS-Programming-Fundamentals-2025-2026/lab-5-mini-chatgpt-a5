3c1650af40db6d002978382977eccbeadffd1ef5
Zhovtiak Maksym <maxon.zhovtyak@gmail.com>
2026-03-28 14:35:21 +0200

    Added A10 part.

---
+++ A5/mini-chatgpt/src/Lib.ChatConsole/Commands/TopKCommand.cs
@@ -0,0 +1,29 @@
+﻿namespace MiniChatGPT.ChatConsole.Commands
+{
+    public class TopKCommand : IReplCommand
+    {
+        public string Name
+        {
+            get { return "/topK"; }
+        }
+        public string Description
+        {
+            get { return "Встановлює TopK для генерації"; }
+        }
+
+        public void Execute(string[] args, CommandExecutionContext context)
+        {
+            if(args.Length > 0 && int.TryParse(args[0], out int newTopK))
+            {
+                context.Options.TopK = newTopK;
+                context.PrintMessage("\nЗначення TopK оновлено");
+
+                context.PrintMessage(context.GetStatusInfo());
+            }
+            else
+            {
+                context.PrintMessage("Вказано некоректне число");
+            }
+        }
+    }
+}
