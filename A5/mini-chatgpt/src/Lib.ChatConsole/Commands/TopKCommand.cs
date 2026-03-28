bcf6f6ae51ccc18b5f2e19eee0aea527c51a18e9
Zhovtiak Maksym <maxon.zhovtyak@gmail.com>
2026-03-28 15:21:07 +0200

    Added tests.

--- A5/mini-chatgpt/src/Lib.ChatConsole/Commands/TopKCommand.cs
+++ A5/mini-chatgpt/src/Lib.ChatConsole/Commands/TopKCommand.cs
@@ -4,7 +4,7 @@
     {
         public string Name
         {
-            get { return "/topK"; }
+            get { return "/topk"; }
         }
         public string Description
         {
@@ -13,7 +13,7 @@
 
         public void Execute(string[] args, CommandExecutionContext context)
         {
-            if(args.Length > 0 && int.TryParse(args[0], out int newTopK))
+            if (args.Length > 0 && int.TryParse(args[0], out int newTopK) && newTopK > 0)
             {
                 context.Options.TopK = newTopK;
                 context.PrintMessage("\nЗначення TopK оновлено");
@@ -22,7 +22,7 @@
             }
             else
             {
-                context.PrintMessage("Вказано некоректне число");
+                context.PrintMessage("Вказано некоректне число (має бути ціле число більше за 0)");
             }
         }
     }
